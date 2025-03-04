adeb3615f2769a649c9def9ba165679adc60aa29
gfavo <gabriel.lfavaro@gmail.com>
2025-03-04 18:53:59 -0300

    Capturing the file and loading it in the workbook

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(ExcelJS);
+async function manipulateXlsx() {
+    const workbook = new ExcelJS.Workbook();
+
+    const input = document.querySelector('input[type=file]');
+    const file = input.files[0];
+
+    const blob = new Blob([file],  {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
+    const buffer = await blob.arrayBuffer();
+
+    await workbook.xlsx.load(buffer);
+}
